ffa15522ffeed8add2008e87da9f1439f41b9822
yekaiwu <119912911+yekaiwu@users.noreply.github.com>
2024-02-28 13:41:01 +0800

    Update Trick.java

--- Trick.java
+++ Trick.java
@@ -1,24 +1,17 @@
 // Variables:
-// List<Card> cardsPlayed: Represents the cards played in the current trick.
-// int currentPlayerIndex: Indicates the index of the current player in the list of players.
-// List<Player> players: Holds the list of players participating in the game.
-// boolean isFirstTrick: Flags whether it's the first trick of the round.
 // int numPoints: Stores the number of points accumulated in the trick.
 // ArrayList<Card> cardsInTrick: Stores the cards currently in the trick.
 // Player playerHoldingTrick: Represents the player who currently holds the trick.
+// Card leadingCardOfTrick: Stores the Card that was played first in a trick.
 
 // Constructors:
-// Trick(List<Player> players, boolean isFirstTrick): Initializes a new Trick object with the specified list of players and whether it's the first trick of the round.
+// Trick(ArrayList<Cards>): Initializes a new Trick object
 
 // Methods:
-// playCard(Player player, Card card): Allows a player to play a card in the current trick.
-// isValidPlay(Player player, Card card): Validates whether the card being played is valid according to the game rules.
-// getWinningCard(): Determines the winning card in the trick based on the cards played and game rules.
 // getPlayerHoldingTrick(): Returns the player who played the winning card.
-// isTrickComplete(): Checks if the trick is complete (i.e., all players have played a card).
 // getNumPoints(): Returns the number of points accumulated in the trick.
 // getCardsInTrick(): Returns the cards currently in the trick.
-// returnPlayableCards(Player player): Returns the playable cards for the given player in the trick.
+// getLeadingCard(): Return the leading card
 
 
 import java.util.ArrayList;
@@ -125,13 +118,4 @@ public class Trick {
         return playerHoldingTrick;
     }
 
-    // Method to return the playable cards for a given player
-    public ArrayList<Card> returnPlayableCards(Player player) {
-        // Implement this method based on the game's rules
-        // You need to determine which cards the player can play in the trick
-        // and return them as an ArrayList<Card>
-        ArrayList<Card> playableCards = new ArrayList<>();
-        // Add logic to determine playable cards
-        return playableCards;
-    }
 }

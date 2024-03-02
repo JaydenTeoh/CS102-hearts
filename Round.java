cc31be3ea7db8c463824979dacf20bffe4bf57f2
vincetyy <97782480+vincetyy@users.noreply.github.com>
2024-03-02 21:33:12 +0800

    added round and hand

--- Round.java
+++ Round.java
@@ -1,7 +1,28 @@
+import java.util.ArrayList;
+
 public class Round {
     private boolean heartsBroken;
+    private Player playerStartingFirst;
+    private int numTricksPlayed;
+    // private Trick currentTrick;
+
+    public Round(Player playerStartingFirst){
+        this.playerStartingFirst = playerStartingFirst;
+    }
+
+    public Player getPlayerStartingFirst() {
+        return playerStartingFirst;
+    }
+
+    public int getNumTricksPlayed() {
+        return numTricksPlayed;
+    }
 
     public boolean isHeartsBroken() {
         return heartsBroken;
     }
+
+    // public startNewTrick(Player player){
+    //     this.currentTrick = new Trick(new ArrayList<Card>(), )
+    // }
 }

c42c6c5147e52a6eddb20fffc488050d77b71e0b
vincetyy <97782480+vincetyy@users.noreply.github.com>
2024-03-03 14:59:16 +0800

    added dealHands() and associated variables to Round, still WIP

--- Round.java
+++ Round.java
@@ -2,15 +2,20 @@ import java.util.ArrayList;
 
 public class Round {
     private boolean heartsBroken;
-    private Player playerStartingFirst;
+    private int playerStartingFirst;
     private int numTricksPlayed;
-    // private Trick currentTrick;
+    private Trick currentTrick;
+    private Game belongsToGame;
+    private ArrayList<Player> players;
 
-    public Round(Player playerStartingFirst){
+    public Round(Player playerStartingFirst, Game belongsToGame){
         this.playerStartingFirst = playerStartingFirst;
+        this.belongsToGame = game;
+        // need to implement Game.getPlayers later on
+        this.players = game.getPlayers();
     }
 
-    public Player getPlayerStartingFirst() {
+    public int getPlayerStartingFirst() {
         return playerStartingFirst;
     }
 
@@ -22,7 +27,36 @@ public class Round {
         return heartsBroken;
     }
 
-    // public startNewTrick(Player player){
-    //     this.currentTrick = new Trick(new ArrayList<Card>(), )
-    // }
+    public startNewTrick(int player){
+        this.currentTrick = new Trick(new ArrayList<Card>(), )
+    }
+
+
+    // Need to add the static variables later on
+    public void dealHands() {
+        ArrayList<Hand> hands = new ArrayList<>();
+
+        // create new deck, then populate 52 standard poker cards and randomise it (shuffle)
+        Deck d = new Deck();
+        d.populate();
+        d.shuffle();
+
+        // create 4 new hands
+        for (int i = 0; i < NUM_PLAYERS; i++) {
+            hands.add(new Hand());
+        }
+
+        // populate 4 hands from the shuffled deck
+        for (int card = 0; card < MAX_NUMBER_OF_CARDS_PER_PLAYER; card++) {
+            for (int handIndex = 0; handIndex < NUM_PLAYERS; handIndex++) {
+                if (!d.isEmpty()) {
+                    hands.get(handIndex).addCard(d.dealCard());
+                }
+            }
+        }
+
+        for (int i = 0; i < NUM_PLAYERS; i++) {
+            players[i].setHand(hands[i]);
+        }
+    }
 }

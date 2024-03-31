d07daff1e407ad2fd155a2717345f2d38c4ed76c
Jayden Teoh <t3ohjingxiang@gmail.com>
2024-03-31 17:49:22 +0800

    remove unused imports in players

--- src/main/java/com/example/players/HumanPlayer.java
+++ src/main/java/com/example/players/HumanPlayer.java
@@ -1,8 +1,6 @@
 package com.example.players;
-import java.util.*;
 
 import com.example.gameplay.*;
-import com.example.pokercards.*;
 
 public class HumanPlayer implements Player {
     public String name;
@@ -23,16 +21,4 @@ public class HumanPlayer implements Player {
     public void setHand(Hand hand) {
         this.hand = hand;
     }
-
-    //@Override
-    // public Card playCard(Round round, Trick trick) {
-    //     // not implemented yet
-    //     return null;
-    // }
-
-    // @Override
-    // public void passCards(List<Card> cards, Player player) {
-    //     hand.getCards().removeAll(cards);
-    //     player.getHand().getCards().addAll(cards);
-    // }
 }
--- src/main/java/com/example/players/Player.java
+++ src/main/java/com/example/players/Player.java
@@ -1,10 +1,5 @@
 package com.example.players;
 import com.example.gameplay.*;
-import java.util.*;
-
-import com.example.exceptions.*;
-import com.example.app.*;
-import com.example.pokercards.*;
 
 public interface Player {
     public String getName();
@@ -12,8 +7,4 @@ public interface Player {
     public Hand getHand();
 
     public void setHand(Hand hand);
-
-    //public abstract Card playCard(Round round, Trick trick);
-
-    //public abstract List<Card> passCards();
 }
